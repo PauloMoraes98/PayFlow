0d822cb6806cde31d399f48c944677e5159deafa
unknown <paulo.csm_@hotmail.com>
2021-08-29 21:28:10 -0300

    Criando a HomePage

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -1,9 +1,3 @@
-//
-// Generated file. Do not edit.
-//
-
-// ignore_for_file: lines_longer_than_80_chars
-
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
